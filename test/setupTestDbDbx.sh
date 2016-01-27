7e2cc54c10cee0561c5cd9910f924f396fe55079
Bill Evans <bill@literacybridge.org>
2016-01-26 21:45:43 -0800

    Added a reminder that setupTestDbDbx should be "sourced".

--- test/setupTestDbDbx.sh
+++ test/setupTestDbDbx.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Sets shell variables for connecting to a local machine test database
+# Source this file to set shell variables for connecting to a local machine test database
 
 export psql="/Applications/Postgres.app/Contents/Versions/9.5/bin/psql"
 export dbcxn="--host=localhost --port 5432 --username=lb_data_uploader --dbname=dashboard"
